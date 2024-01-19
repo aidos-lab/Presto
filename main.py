44694688a1a5aa8de317c57d9877f56e229f7a00
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-01-19 15:56:44 +0100

    formatting outputs

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import time
+
 import numpy as np
 
 from pesto import PESTO
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     metric = PESTO(max_homology_dim=2, normalize=True)
 
-    X = np.random.random(size=(5000, 10))
-    Y = np.random.random(size=(5000, 8))
+    X = np.random.random(size=(15000, 100))
+    Y = np.random.random(size=(10000, 256))
+    N = 10
+    print("--------------------------")
+    print(f"Embedding 1: {X.shape}")
+    print(f"Embedding 2: {Y.shape}")
+    print("--------------------------")
 
-    score = metric.fit_transform(X, Y, N=15)
+    print()
+    print(f"Aggregating Landsacpes for {N} Random Projections...")
+    score = metric.fit_transform(X, Y, N=N)
 
-    print(score)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print("Execution time: {:.2f} seconds".format(elapsed_time))
+    print("PESTO Score: {:.4f}".format(np.round(score, 4)))
