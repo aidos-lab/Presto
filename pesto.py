a8082fa065e0dfa8a79b85770bf5427dd8697b9e
dataspider <corinna.coupette@gmail.com>
2024-01-20 15:07:14 +0100

    p(r)esto bug fixes and unit tests

--- pesto.py
+++ pesto.py
@@ -9,13 +9,14 @@ from sklearn.random_projection import GaussianRandomProjection as Gauss
 
 class PESTO:
     def __init__(
-        self,
-        projector=Gauss,
-        n_components=2,
-        normalize: bool = False,
-        max_homology_dim: int = 1,
-        resolution: int = 100,
-        normalization_approx_iterations: int = 1000,
+            self,
+            projector=Gauss,
+            n_components=2,
+            normalize: bool = False,
+            max_homology_dim: int = 1,
+            resolution: int = 100,
+            normalization_approx_iterations: int = 1000,
+            seed: int = 42
     ) -> None:
         """
         Initialize `PESTO` an object for efficienctly computing the structural similarity of embeddings.
@@ -34,27 +35,34 @@ class PESTO:
             The resolution parameter for computing persistence landscapes. Default is 100.
         - normalization_approx_iterations : int, optional
             The number of iterations for approximating the space diameter during normalization. Default is 1000.
+        - seed : int, optional
+            Seed for the random number generator. Default is 42.
         """
+        # Create random number generator
+        self.seed = seed
+        self.rng = np.random.default_rng(self.seed)
+
         # Initialize Projector
         self.projection_dimension = n_components
-        self.P = projector(n_components=self.projection_dimension)
+        self.P = projector(n_components=self.projection_dimension, random_state=self.seed)
 
         # Set Normalization parameters
         self.normalize = normalize
         self.diameter_iterations = normalization_approx_iterations
 
         # Set Topological parameters
-        self.homology_dims = range(0, max_homology_dim + 1)
+        self.homology_dims = list(range(0, max_homology_dim + 1))
         self.landscape_resolution = resolution
-        self.LS = Landscape(resolution=self.landscape_resolution)
+        self.LS = Landscape(resolution=self.landscape_resolution, keep_endpoints=False)
         self._landscapeX = None
         self._landscapeY = None
+        self._pestos = None
 
     def fit(
-        self,
-        X,
-        Y,
-        N=15,
+            self,
+            X,
+            Y,
+            N=15,
     ):
         """
         Fit a topological descriptor to embeddings X & Y.
@@ -90,7 +98,7 @@ class PESTO:
         self._landscapeX = self._average_landscape(self._all_landscapesX)
         self._landscapeY = self._average_landscape(self._all_landscapesY)
 
-    def fit_transform(self, X, Y, N=15):
+    def fit_transform(self, X, Y, N: int = 15, score_type: str = "aggregate"):
         """
         Fit a topological descriptor and compute the PESTO score.
 
@@ -101,22 +109,39 @@ class PESTO:
             The second set of embeddings.
         - N : int, optional
             The number of random projections. Default is 100.
+        - score_type: str, optional
+            Which type of PESTO score to return. Options are:
+            - "aggregate" (sum normed distances across all dimensions)
+            - "average" (average distance across al dimensions)
+            - "separate" (list of distances by dimension)
+            Default is "aggregate".
 
         Returns:
         - pesto_score : float
             The computed PESTO score representing the distance between the topological descriptors of X and Y.
         """
-        if not self._landscapeX:
+        self._set_pestos(X, Y, N)
+        if score_type == "aggregate":
+            return sum(self._pestos.values())
+        elif score_type == "average":
+            return sum(self._pestos.values()) / len(self._pestos.values())
+        elif score_type == "separate":
+            return self._pestos
+        else:
+            raise NotImplementedError(score_type)
+
+    def _set_pestos(self, X, Y, N: int = 15):
+        if self._landscapeX is None or self._landscapeY is None:
             self.fit(X, Y, N)
 
         assert self._landscapeX is not None
-        pesto_score = 0
+        pestos = dict()
         for dim in self.homology_dims:
             lambdaX = self._landscapeX[dim]
             lambdaY = self._landscapeY[dim]
             if not np.isnan(lambdaX - lambdaY).any():
-                pesto_score += np.linalg.norm(lambdaX - lambdaY)
-        return pesto_score
+                pestos[dim] = np.linalg.norm(lambdaX - lambdaY)
+        self._pestos = pestos
 
     def _normalize_space(self, X):
         """
@@ -130,7 +155,7 @@ class PESTO:
         - normalized_X : np.ndarray
             The normalized space.
         """
-        subset = [np.random.choice(len(X))]
+        subset = [self.rng.random.choice(len(X))]
         for _ in range(self.diameter_iterations - 1):
             distances = cdist([X[subset[-1]]], X).ravel()
             new_point = np.argmax(distances)
@@ -171,18 +196,18 @@ class PESTO:
         - landscapes : dict
             Dictionary containing persistence landscapes for each homology dimension.
         """
-        landscapes = {}
-        for dim in self.homology_dims:
-            for X_ in projections:
-                alpha_complex = gd.AlphaComplex(points=X_).create_simplex_tree()
-                # Compute Peristence
-                alpha_complex.persistence()
-                persistence_pairs = alpha_complex.persistence_intervals_in_dimension(
+        landscapes = {dim: list() for dim in self.homology_dims}
+        # all_persistence_pairs = {dim: list() for dim in self.homology_dims}
+        for X_ in projections:
+            alpha_complex = gd.AlphaComplex(points=X_).create_simplex_tree()
+            # Compute Peristence
+            alpha_complex.persistence()
+            for dim in self.homology_dims:
+                persistence_pairs = mask_infinities(alpha_complex.persistence_intervals_in_dimension(
                     dim
-                )
-                # TODO debug it looks like you're overwriting, rather than appending to a list – that could explain the volatility I observed
-                # Also, I get only nans for 0th dimension and only 0s for 2nd dimension, so only 1st dimension appears to "work"/be meaningful?
-                landscapes[dim] = self.LS.fit_transform([persistence_pairs])
+                ))
+                # all_persistence_pairs[dim].append(persistence_pairs)
+                landscapes[dim].append(self.LS.fit_transform([persistence_pairs]))
         return landscapes
 
     def _average_landscape(self, L: dict):
@@ -205,3 +230,7 @@ class PESTO:
                 sum_ += l
             avg[dim] = sum_.__truediv__(N)
         return avg
+
+
+def mask_infinities(array):
+    return array[array[:, 1] < np.inf]
---
+++ tests/test_pesto.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+from pesto import PESTO
+from sklearn.random_projection import GaussianRandomProjection as Gauss
+
+
+class PestoTest(TestCase):
+    def setUp(self) -> None:
+        self.projector = Gauss
+        self.n_components = 3
+        self.normalize = False
+        self.max_homology_dim = 2
+        self.resolution = 100
+        self.normalization_approx_iterations = 1000
+        self.seed = 42
+        self.pesto = PESTO(projector=self.projector, n_components=self.n_components, normalize=self.normalize,
+                           max_homology_dim=self.max_homology_dim, resolution=self.resolution,
+                           normalization_approx_iterations=self.normalization_approx_iterations, seed=self.seed)
+        self.n_projections = 15
+        self.X = self.pesto.rng.random(size=(100, 1000))
+        self._projectionsX = self.pesto._generate_projections(self.X, self.n_projections)
+        self._landscapesX = self.pesto._generate_landscapes(self._projectionsX)
+        self._landscapeX = self.pesto._average_landscape(self._landscapesX)
+        self.Y = self.pesto.rng.random(size=(200, 500))
+
+    def test_homology_dims(self):
+        self.assertListEqual([0, 1, 2], self.pesto.homology_dims)
+
+    def test_generate_projections(self):
+        self.assertEqual(self.n_projections, len(self._projectionsX))
+        self.assertEqual(self.X.shape[0], self._projectionsX[0].shape[0])
+        self.assertEqual(self.n_components, self._projectionsX[0].shape[1])
+
+    def test_generate_landscapes(self):
+        self.assertEqual(self.max_homology_dim, len(self._landscapesX) - 1)
+        self.assertEqual(self.n_projections, len(self._landscapesX[0]))
+
+    def test_average_landscape(self):
+        self.assertEqual(self.max_homology_dim, len(self._landscapeX) - 1)
+
+    def test_set_pestos(self):
+        self.pesto._set_pestos(self.X, self.Y, self.n_projections)
+
+    def test_normalize_space(self):
+        pass
+
+    def test_fit(self):
+        pass
+
+    def test_fit_transform(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
