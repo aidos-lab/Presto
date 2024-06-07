5098adfaee21c86f5b7113024c48835aa61d2e29
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-07 18:15:20 +0200

    dim reducuction configuration for LSD loading

---
+++ lsd/generate/dim_reductions/__init__.py
@@ -0,0 +1,11 @@
+from .configs import UMAP, tSNE, Phate, MNIST, Thread
+from .dr import DimReduction
+
+__all__ = [
+    "DimReduction",
+    "UMAP",
+    "tSNE",
+    "Phate",
+    "MNIST",
+    "Thread",
+]
