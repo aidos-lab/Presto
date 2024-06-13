b08228d0483f3b564b4e82f50de48d465d11e19d
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-13 16:39:41 +0200

    `UMAPProjector` udpate

--- lsd/generate/dim_reductions/models/umap.py
+++ lsd/generate/dim_reductions/models/umap.py
@@ -1,31 +1,24 @@
-from dataclasses import dataclass
-
 from umap import UMAP
 
-from ..dr import DimReductionModule
+from lsd.generate.dim_reductions.configs import Projector
 
 
-# @dataclass
-# class UMAPConfig(DimReductionConfig):
-#     n_neighbors: int = 15
-#     min_dist: float = 0.1
-#     init: str = "spectral"
-#     metric: str = "euclidean"
-#     dim: int = 2
-
-
-class UMAPProjector(DimReductionModule):
-    def __init__(self, config: UMAPConfig):
+class UMAPProjector:
+    def __init__(self, config: Projector):
         super().__init__(config)
 
     def project(self, data):
         operator = UMAP(
-            n_neighbors=self.config.n_neighbors,
+            n_neighbors=self.config.nn,
             min_dist=self.config.min_dist,
-            n_components=self.config.dim,
-            metric=self.config.metric,
+            n_components=self.dim,
+            metric=self.metric,
             init=self.config.init,
             random_state=self.config.seed,
         )
 
         return operator.fit_transform(data)
+
+
+def initialize():
+    return UMAPProjector
