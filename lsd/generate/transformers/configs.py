fc9fb097807ba29d994930f2f756e99cd30a28fa
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-20 11:07:09 +0200

    Transformer stub

--- lsd/generate/transformers/configs.py
+++ lsd/generate/transformers/configs.py
@@ -3,24 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class Mistral:
-    name: str = "Mistral TEST"
-
-
-@dataclass
-class Ada:
-    name: str = "ADA TEST"
-
-
-@dataclass
-class MiniLM:
-    name = "MiniLM TEST"
-
-
-@dataclass
-class arXiv:
-    name = "arXiv TEST"
-
-
-@dataclass
-class CNN:
-    name = "CNN TEST"
+    name: str = "Mistral AI"
--- lsd/generate/transformers/tf.py
+++ lsd/generate/transformers/tf.py
@@ -4,34 +4,15 @@ from lsd import Base
 class Transformer(Base):
     def __init__(self, params: dict):
         super().__init__(params)
+        raise NotImplementedError(
+            "Transformer Generator not implemented–coming soon!."
+        )
 
     def setup(self):
-        "process and unpack the parameters into their proper configs."
-        # Find proper models and datasets
-        # write proper config files
         pass
 
     def train(self):
         pass
 
     def generate(self):
-        "Use a pretrained model to generate a latent space."
-        pass
-
-
-class Custom(Base):
-    def __init__(self, params: dict):
-        super().__init__(params)
-
-    def setup(self):
-        "process and unpack the parameters into their proper configs."
-        # Find proper models and datasets
-        # write proper config files
-        pass
-
-    def train(self):
-        pass
-
-    def generate(self):
-        "Use a pretrained model to generate a latent space."
         pass
