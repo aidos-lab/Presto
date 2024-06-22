fad332b21530b2d4917c19725db16fde775aba05
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-22 20:46:46 +0200

    chore: Remove unused code for writing and reading files

--- lsd/base.py
+++ lsd/base.py
@@ -22,18 +22,3 @@ class Base(ABC):
     @abstractmethod
     def generate(self):
         pass
-
-    @staticmethod
-    def write_pkl(data, path):
-        with open(path, "wb") as f:
-            pickle.dump(data, f)
-
-    @staticmethod
-    def read(path):
-        if path.endswith(".npz"):
-            return np.load(path)
-        elif path.endswith(".pkl"):
-            with open(path, "rb") as f:
-                return pickle.load(f)
-        else:
-            raise NotImplementedError(f"File type not supported: {path}")
