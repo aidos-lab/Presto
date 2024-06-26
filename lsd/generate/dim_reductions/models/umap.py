cde89eced9fb964c1e5c0b6558538582a9579dc7
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-26 15:26:29 +0200

    refactor: Update UMAPProjector class with type annotations and documentation

--- lsd/generate/dim_reductions/models/umap.py
+++ lsd/generate/dim_reductions/models/umap.py
@@ -1,24 +1,65 @@
 from umap import UMAP
 
-from lsd.generate.dim_reductions.configs import Projector
+from lsd.generate.dim_reductions.configs import UMAP as UMAPConfig
 from lsd.generate.dim_reductions.models.projector import BaseProjector
 
 
 class UMAPProjector(BaseProjector):
-    def __init__(self, config: Projector):
-        super(UMAPProjector, self).__init__(config)
+    """
+    Class for performing dimensionality reduction using Uniform Manifold Approximation and Projection (UMAP).
+
+    Inherits from BaseProjector, which provides common functionality for projectors.
+
+    Attributes
+    ----------
+    config : UMAPPConfig
+        Configuration object containing parameters for t-SNE projection.
+
+    Methods
+    -------
+    project(data)
+        Perform dimensionality reduction on input data using UMAP.
+
+    """
+
+    def __init__(self, config: UMAPConfig):
+        """
+        Constructor method for UMAPProjector class.
+
+        Parameters
+        ----------
+        config : Projector
+            Configuration object containing parameters for UMAP projection.
+        """
+        super().__init__(config)
 
     def project(self, data):
-        operator = UMAP(
-            n_neighbors=self.config.nn,
-            min_dist=self.config.min_dist,
-            n_components=self.dim,
-            metric=self.metric,
-            init=self.config.init,
-        )
+        """
+        Perform dimensionality reduction on input data using Uniform Manifold Approximation and Projection (UMAP).
 
+        Parameters
+        ----------
+        data : array-like of shape (n_samples, n_features)
+            Input data to be transformed.
+
+        Returns
+        -------
+        np.ndarray
+            UMAP embedding of shape (n_samples,self.config.n_components).
+        """
+        params = self._get_parameters(UMAP, self.config)
+        operator = UMAP(**params)
         return operator.fit_transform(data)
 
 
-def initialize():
+def initialize() -> UMAPProjector:
+    """
+    Function to initialize an instance of UMAPProjector.
+
+    Returns
+    -------
+    UMAPProjector
+        Instance of UMAPProjector class.
+
+    """
     return UMAPProjector
