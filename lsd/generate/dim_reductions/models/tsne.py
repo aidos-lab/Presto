dec72700580bcdcdf102938364f8bdd2c235e244
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-26 15:26:24 +0200

    refactor: Update TSNEProjector class with type annotations and documentation

--- lsd/generate/dim_reductions/models/tsne.py
+++ lsd/generate/dim_reductions/models/tsne.py
@@ -1,23 +1,68 @@
+import numpy as np
 from sklearn.manifold import TSNE
 
 from lsd.generate.dim_reductions.models.projector import BaseProjector
+from lsd.generate.dim_reductions.configs import tSNE as tSNEConfig
 
 
 class TSNEProjector(BaseProjector):
-    def __init__(self, config):
-        super(TSNEProjector, self).__init__(config)
+    """
+    Class for performing dimensionality reduction using t-Distributed Stochastic Neighbor Embedding (t-SNE).
 
-    def project(self, data):
-        operator = TSNE(
-            perplexity=self.config.perplexity,
-            early_exaggeration=self.config.ee,
-            n_components=self.dim,
-            metric=self.metric,
-            random_state=self.config.seed,
-        )
+    Inherits from BaseProjector, which provides common functionality for projectors.
+
+    Attributes
+    ----------
+    config : tSNEConfig
+        Configuration object containing parameters for t-SNE projection.
+
+    Methods
+    -------
+    project(data)
+        Perform dimensionality reduction on input data using t-SNE.
+
+    """
+
+    def __init__(self, config: tSNEConfig):
+        """
+        Constructor method for TSNEProjector class.
+
+        Parameters
+        ----------
+        config : tSNEConfig
+            Configuration object containing parameters for t-SNE projection.
+        """
+        super().__init__(config)
+
+    def project(self, data) -> np.ndarray:
+        """
+        Perform dimensionality reduction on input data using t-Distributed Stochastic Neighbor Embedding (t-SNE).
+
+        Parameters
+        ----------
+        data : array-like of shape (n_samples, n_features)
+            Input data to be transformed.
+
+        Returns
+        -------
+        np.ndarray
+            tSNE embedding of shape (n_samples,self.config.n_components).
+
+        """
+        params = self._get_parameters(TSNE, self.config)
+        operator = TSNE(**params)
 
         return operator.fit_transform(data)
 
 
-def initialize():
+def initialize() -> TSNEProjector:
+    """
+    Function to initialize an instance of TSNEProjector.
+
+    Returns
+    -------
+    TSNEProjector
+        Instance of TSNEProjector class.
+
+    """
     return TSNEProjector
