6780ac42d7be41e698bf07c720693f98aa9ff37f
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-26 15:33:39 +0200

    update BaseProjector documentation

--- lsd/generate/dim_reductions/models/projector.py
+++ lsd/generate/dim_reductions/models/projector.py
@@ -1,26 +1,81 @@
 from abc import ABC, abstractmethod
-from lsd.generate.dim_reductions.configs import Projector
 from inspect import signature
+import numpy as np
+
+from lsd.generate.dim_reductions.configs import Projector as ProjectorConfig
 
 
 class BaseProjector(ABC):
-    def __init__(self, config: Projector):
+    """
+    Base class for all projector classes.
+
+    This abstract base class provides a common interface for all projector implementations.
+
+    Attributes
+    ----------
+    config : Projector
+        The configuration object containing parameters for the projection algorithm.
+
+    Methods
+    -------
+    project(data)
+        Abstract method to perform the projection on the provided data. Must be implemented by subclasses.
+
+    _get_parameters(Operator, config) -> dict
+        Static method to extract relevant parameters from the configuration for the given operator.
+    """
+
+    def __init__(self, config: ProjectorConfig):
+        """
+        Constructor method for BaseProjector class.
+
+        Parameters
+        ----------
+        config : Projector
+            The configuration object containing parameters for the projection.
+        """
         self.config = config
 
-    @property
-    def dim(self):
-        return self.config.n_components
-
-    @property
-    def metric(self):
-        return self.config.metric
-
     @abstractmethod
-    def project(self, data):
+    def project(self, data) -> np.ndarray:
+        """
+        Abstract method to project the data into a lower-dimensional space.
+
+        This method must be implemented by subclasses to perform the actual
+        dimensionality reduction based on the specific algorithm.
+
+        Parameters
+        ----------
+        data : array-like
+            The input data to be projected.
+
+        Returns
+        -------
+        np.ndarray
+            The projected data.
+        """
         raise NotImplementedError()
 
     @staticmethod
     def _get_parameters(Operator, config) -> dict:
+        """
+        Extracts the relevant parameters from the configuration for the given operator.
+
+        This static method uses introspection to retrieve the parameters of the operator's
+        `__init__` method and matches them with the corresponding values in the configuration.
+
+        Parameters
+        ----------
+        Operator : class
+            The projection operator class whose parameters are to be retrieved.
+        config : Projector
+            The configuration object containing parameters for the projection.
+
+        Returns
+        -------
+        dict
+            A dictionary of parameters for the operator, populated with values from the configuration.
+        """
         params = signature(Operator.__init__).parameters
         args = {
             name: config.get(name, param.default)
@@ -30,5 +85,16 @@ class BaseProjector(ABC):
         return args
 
 
-def initialize():
+def initialize() -> BaseProjector:
+    """
+    Initializes and returns the BaseProjector class.
+
+    This function is intended to provide a convenient way to
+    get an instance of the BaseProjector class.
+
+    Returns
+    -------
+    BaseProjector
+        The BaseProjector class.
+    """
     return BaseProjector
