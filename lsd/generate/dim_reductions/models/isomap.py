e2944c8041444f7c68f37dda6a9971985cabc047
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-26 15:25:32 +0200

    refactor: Update IsomapProjector class with type annotations and documentation

--- lsd/generate/dim_reductions/models/isomap.py
+++ lsd/generate/dim_reductions/models/isomap.py
@@ -1,21 +1,67 @@
+import numpy as np
 from sklearn.manifold import Isomap
 
-
 from lsd.generate.dim_reductions.models.projector import BaseProjector
+from lsd.generate.dim_reductions.configs import Isomap as IsomapConfig
 
 
 class IsomapProjector(BaseProjector):
-    def __init__(self, config):
+    """
+    Class for performing dimensionality reduction using Isometric Mapping (Isomap).
+
+    Inherits from BaseProjector, which provides common functionality for projectors.
+
+    Attributes
+    ----------
+    config : IsomapConfig
+        Configuration object containing parameters for Isomap projection.
+
+    Methods
+    -------
+    project(data)
+        Perform dimensionality reduction on input data using Isomap.
+
+    """
+
+    def __init__(self, config: IsomapConfig):
+        """
+        Constructor method for IsomapProjector class.
+
+        Parameters
+        ----------
+        config : IsomapConfig
+            Configuration object containing parameters for Isomap projection.
+        """
         super().__init__(config)
 
-    def project(self, data):
-        operator = Isomap(
-            n_neighbors=self.config.nn,
-            n_components=self.dim,
-            metric=self.metric,
-        )
+    def project(self, data) -> np.ndarray:
+        """
+        Perform dimensionality reduction on input data using Isomap.
+
+        Parameters
+        ----------
+        data : array-like of shape (n_samples, n_features)
+            Input data to be transformed.
+
+        Returns
+        -------
+        np.ndarray
+            ISOMAP embedding of shape (n_samples,self.config.n_components).
+
+        """
+        params = self._get_parameters(Isomap, self.config)
+        operator = Isomap(**params)
         return operator.fit_transform(data)
 
 
-def initialize():
+def initialize() -> IsomapProjector:
+    """
+    Function to initialize an instance of IsomapProjector.
+
+    Returns
+    -------
+    IsomapProjector
+        Instance of IsomapProjector class.
+
+    """
     return IsomapProjector
